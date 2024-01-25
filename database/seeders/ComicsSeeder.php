5b7e21fab9ffa51659d821d9ffd41ecbb80b2951
Eugen <epopescuu@yahoo.it>
2024-01-25 18:15:12 +0100

    perf: add seeder and manupulated database

---
+++ database/seeders/ComicsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $array_comic = config("data.fumetti");
+
+        foreach ($array_comic as $comic_item) {
+            $new_comic = new Comic();
+            $new_comic->title = $comic_item["title"];
+            $new_comic->description = $comic_item["description"];
+            $new_comic->thumb = $comic_item["thumb"];
+            $new_comic->price = $comic_item["price"];
+            $new_comic->series = $comic_item["series"];
+            $new_comic->sale_date = $comic_item["sale_date"];
+            $new_comic->type = $comic_item["type"];
+            $new_comic->save();
+        }
+    }
+}
